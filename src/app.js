a5e35906a3d6ee318836e453f0b4d3b5e16793f4
sbyuxD ! <irmalove999@gmail.com>
2026-09-03 22:17:31 +0700

    upload: src/app.js

--- src/app.js
+++ src/app.js
@@ -23,6 +23,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+  res.setHeader("Surrogate-Control", "no-store");
+  next();
+});
+
 try {
   app.use(express.static(path.resolve(__dirname, "../public")));
   app.use("/media", express.static(path.resolve(__dirname, "media")));
